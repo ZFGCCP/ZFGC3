745e9f26a7c5783fbfa6d6f0dcdf3dbdcfaa6d8a
Daniel Caruso <dcaruso19@gmail.com>
2017-01-07 16:57:35 -0500

    forum index loading data

--- src/main/webapp/scripts/forum/forum.controller.js
+++ src/main/webapp/scripts/forum/forum.controller.js
@@ -3,10 +3,11 @@
 	
 	function ForumController(LookupsService, UserService, ForumService){
 		var vm = this;
-		
+		vm.categories = [];
 		
 		vm.logIndex = function(){
 			ForumService.resource.ForumIndex().$promise.then(function(data){
+				vm.categories=data.categories;
 				console.log(data);
 			});
 		};
