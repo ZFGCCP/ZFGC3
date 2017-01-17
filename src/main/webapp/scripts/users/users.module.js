48c737c6c1dfeda3d7e24925c0a125630ddebc41
mgzero <onisephiroth@gmail.com>
2017-01-16 23:11:47 -0500

    starting profile

--- src/main/webapp/scripts/users/users.module.js
+++ src/main/webapp/scripts/users/users.module.js
@@ -6,6 +6,10 @@ angular.module('zfgc.users',['zfgc.config'])
 				url: '/users',
 				templateUrl: basePath + '/users.html'
 			})
+			.state('user-profile', {
+				url: '/userprofile',
+				templateUrl: basePath + '/user-profile/user-profile.html'
+			})
 			.state('registration', {
 				url: '/registration',
 				templateUrl: basePath + '/registration.html'
