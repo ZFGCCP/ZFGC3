efdaf394764f052cfbcc785b36d3e0c711d64cb5
mgzero <onisephiroth@gmail.com>
2017-01-23 22:01:05 -0500

    working on directive fixes

--- src/main/webapp/scripts/directives/collapsible-widget/collapsible-widget.directive.js
+++ src/main/webapp/scripts/directives/collapsible-widget/collapsible-widget.directive.js
@@ -4,7 +4,7 @@
 	function collapsibleWidget(){
 		var directive = {};
 		directive.restrict = 'E';
-		directive.replace = true;
+		directive.transclude = true;
 		directive.templateUrl = "scripts/directives/collapsible-widget/collapsible-widget.html";
 		directive.scope = {
 				widgetId :"@",
@@ -16,7 +16,12 @@
 		directive.link = function ($scope, element, attrs) 
 		{ 
 			$scope.vm = $scope.contentModel;
-		};
+			$scope.isCollapsed = false;
+			
+			$scope.toggle = function(){
+				$scope.isCollapsed = !$scope.isCollapsed;
+			};
+		};;
 		
 		return directive;
 	};
