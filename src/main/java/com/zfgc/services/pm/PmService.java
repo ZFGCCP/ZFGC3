795fc13818c2aae58421906890256d6850fc01b0
mgzero <onisephiroth@gmail.com>
2018-09-03 01:05:20 -0400

    added bean mapper to abstract service

--- src/main/java/com/zfgc/services/pm/PmService.java
+++ src/main/java/com/zfgc/services/pm/PmService.java
@@ -510,19 +510,8 @@ public class PmService extends AbstractService {
 		archiveBox.setConversations(new ArrayList<>());
 		
 		for(PmArchiveBoxView archived : archive){
-			PmConversationView convo = new PmConversationView();
-			convo.setMessage(archived.getMessage());
-			convo.setSubject(archived.getSubject());
-			convo.setPmConversationId(archived.getPmConversationId());
-			convo.setPersonalMessageId(archived.getPersonalMessageId());
-			convo.setSentDt(archived.getSentDt());
-			convo.setReceiverId(archived.getReceiverId());
-			convo.setSenderId(archived.getSenderId());
-			convo.setSenderName(archived.getSenderName());
+			PmConversationView convo = mapper.map(archived, PmConversationView.class);
 			convo.setArchived(true);
-			convo.setInitiatorId(archived.getInitiatorId());
-			convo.setInitiatorName(archived.getInitiatorName());
-			convo.setStartDt(archived.getStartDt());
 			
 			archiveBox.getConversations().add(convo);
 		}
