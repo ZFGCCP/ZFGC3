aac583bad59f57a70a9efc1676549b05a7e70ca4
mgzero <onisephiroth@gmail.com>
2019-01-25 00:09:54 -0500

    forward controller updates

--- src/main/java/com/zfgc/controller/ForwardController.java
+++ src/main/java/com/zfgc/controller/ForwardController.java
@@ -1,12 +1,16 @@
 package com.zfgc.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ForwardController{
 
-    @RequestMapping(value = "/{[path:[^\\.]*}")
+	//ignore paths starting with /ws - websockets should not pass through here
+    @RequestMapping(value = "/{^(ws)**}/{[path:[^\\.]*}")
     public String redirect() {
         // Forward to home page so that route is preserved.
         return "forward:/";
