e54e1d8fbf7ebb40bbe853e21ade47eaa58f33b0
mgzero <onisephiroth@gmail.com>
2019-05-27 13:49:59 -0400

    timezone database config

---
+++ src/main/java/com/zfgc/services/system/SystemService.java
@@ -0,0 +1,28 @@
+package com.zfgc.services.system;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.zfgc.dataprovider.SystemSettingsDataProvider;
+import com.zfgc.services.AbstractService;
+import com.zfgc.services.lookups.LookupService;
+
+@Component
+public class SystemService extends AbstractService{
+
+	@Autowired
+	private LookupService lookups;
+	
+	@Autowired
+	private SystemSettingsDataProvider systemSettingsDataProvider;
+	
+	public static String TIME_ZONE = "";
+	
+	@PostConstruct
+	public void initializeSystemSettings() {
+		TIME_ZONE = lookups.getLkupValue("TIMEZONE", Integer.parseInt(systemSettingsDataProvider.getSystemValue("TIME_ZONE")));
+	}
+	
+}
