1d6f2647a3c2a6117f30e2b92e9a45c3346279bf
MGZero <onisephiroth@gmail.com>
2019-06-07 22:27:16 -0400

    birth date rule fix

--- src/main/java/com/zfgc/rules/users/ProfileRuleChecker.java
+++ src/main/java/com/zfgc/rules/users/ProfileRuleChecker.java
@@ -12,12 +12,12 @@ public class ProfileRuleChecker extends AbstractRulesChecker<UserProfileView>{
 
 	@Override
 	public void rulesCheck(UserProfileView model, Users user) throws RuntimeException {
-		/*if(model.getAge() < 13){
+		if(model.getPersonalInfo().getAge() < 13){
 			Rule coppaViolation = new Rule();
 			coppaViolation.setRuleName("COPPA_VIOLATION_AGE");
 			coppaViolation.setErrorMessage("You must be 13 years of age or older to use this forum.");
 			model.getErrors().getRuleErrors().add(coppaViolation);
-		}*/
+		}
 		
 	}
 	
