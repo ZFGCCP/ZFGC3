619d146e2a41ef135f93b7700ddfb2e6fff18d47
mgzero <onisephiroth@gmail.com>
2019-12-15 01:48:46 -0500

    updates email service to format html

--- src/main/java/com/zfgc/util/ZfgcEmailUtils.java
+++ src/main/java/com/zfgc/util/ZfgcEmailUtils.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import it.ozimov.springboot.mail.model.Email;
@@ -19,35 +23,39 @@ import it.ozimov.springboot.mail.service.EmailService;
 
 @Component
 public class ZfgcEmailUtils{
+	
 	@Autowired
-	private EmailService emailService;
+    private JavaMailSender javaMailSender;
 	
 	@Value("${spring.mail.username}") String fromEmail;
-	public void testSendEmail() throws UnsupportedEncodingException { 
-
-        List<InternetAddress> address = new ArrayList<>();
-        address.add(new InternetAddress(
-                "onisephiroth@gmail.com", "zfgc"));
-        final Email email = DefaultEmail.builder() 
-            .from(new InternetAddress(fromEmail, "From Name"))
-            .to(address) 
-            .subject("Testing email")
-            .body("Testing body ...")
-            .encoding("UTF-8").build();
-        emailService.send(email); 
-    }
 	
 	public void sendEmail(String subject, String body, InternetAddress ... to) throws UnsupportedEncodingException{
 		List<InternetAddress> toList = new ArrayList<>();
 		CollectionUtils.addAll(toList, to);
 		InternetAddress from = new InternetAddress(fromEmail, "ZFGC");
 		
-		final Email email = DefaultEmail.builder() 
+		MimeMessage mail = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
+		try {
+			helper = new MimeMessageHelper(mail, true);
+			helper.setTo(to);
+	        helper.setSubject(subject);
+	        helper.setText(body, true);
+	        helper.setFrom(from);
+	        javaMailSender.send(mail);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+        
+        
+		
+		/*final Email email = DefaultEmail.builder() 
 	            .from(from)
 	            .to(toList) 
 	            .subject(subject)
 	            .body(body)
+	            
 	            .encoding("UTF-8").build();
-	        emailService.send(email); 
+	        emailService.send(email);*/
 	}
 }
